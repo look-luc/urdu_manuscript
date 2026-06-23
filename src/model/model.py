c40c19e034b48c35d27976730aeea89da7ad7a3c
look-luc <denardiluc@gmail.com>
2026-06-23 10:55:02 -0600

    getting model ready

--- src/model/model.py
+++ src/model/model.py
@@ -13,9 +13,32 @@ class text_excraction():
     ) -> None:
         self.model_id = model_id
         self.prompt = prompt
+        self.device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_built() else "cpu"
 
-    def extract(self, image:str):
-        self.model = Qwen2VLForConditionalGeneration.from_pretrained(
+    def setup(self):
+        model = Qwen2VLForConditionalGeneration.from_pretrained(
             self.model_id, torch_dtype=torch.float16, device_map="auto"
         )
-        self.processor = AutoProcessor.from_pretrained(self.model_id)
+        processor = AutoProcessor.from_pretrained(self.model_id)
+
+        return model, processor
+
+    def extract(self, image_path:str):
+        self.image_path = image_path
+        image_tensor = io.read_image(self.image_path)
+
+        self.model, self.processor = self.setup()
+
+        self.message = [
+            {
+                "role": "user",
+                "content": [
+                    {"type": "image", "image": image_tensor},
+                    {"type": "text", "text": self.prompt}
+                ]
+            }
+        ]
+
+        self.text = self.processor.apply_chat_template(self.message, tokenize=False, add_generation_prompt=True)
+
+        self.inputs = self.processor(text=[self.text], images=image_tensor, padding=True, return_tensors="pt").to(self.device)
