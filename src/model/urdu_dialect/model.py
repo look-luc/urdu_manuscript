88691e43f6ec8376aac7f9a36828426893b51b58
look-luc <denardiluc@gmail.com>
2026-06-24 17:09:09 -0600

    starting to get set up ready

--- src/model/urdu_dialect/model.py
+++ src/model/urdu_dialect/model.py
@@ -1,3 +1,20 @@
+import torch
+from transformers import AutoModelForVision2Seq, AutoProcessor
+
+
 class unification_urdu_lang_model:
     def __init__(self)->None:
-        self.model_id="Qwen/Qwen2-VL-7B"
+        torch.backends.cudnn.enabled = False
+        self.model_id="Qwen/Qwen3.5-9B"
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model, self.processor = self._setup()
+
+    def _setup (self):
+        model = AutoModelForVision2Seq.from_pretrained(
+            model_id,
+            torch_dtype=torch.bfloat16,
+            device_map="auto"
+        )
+        processor = AutoProcessor.from_pretrained(model_id)
+
+        return  model, processor
