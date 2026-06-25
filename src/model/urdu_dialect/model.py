cdcc4ef833965eef89c7b4f6c02fd5687e96b09f
look-luc <denardiluc@gmail.com>
2026-06-25 12:50:20 -0600

    working on modle setup

--- src/model/urdu_dialect/model.py
+++ src/model/urdu_dialect/model.py
@@ -1,20 +1,55 @@
 import torch
-from transformers import AutoModelForVision2Seq, AutoProcessor
+from transformers import (
+    AutoProcessor,
+    Qwen2VLForConditionalGeneration,
+)
 
 
 class unification_urdu_lang_model:
-    def __init__(self)->None:
+    def __init__(
+        self,
+        model_id:str="Qwen/Qwen2.5-VL-72B-Instruct",
+        prompt:str="""
+            You are a multilingual OCR model. You must extract the text given to you.
+        """
+    )->None:
         torch.backends.cudnn.enabled = False
-        self.model_id="Qwen/Qwen3.5-9B"
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        self.model_id=model_id
         self.model, self.processor = self._setup()
 
+        self.prompt = prompt
+
     def _setup (self):
-        model = AutoModelForVision2Seq.from_pretrained(
-            model_id,
+        model = Qwen2VLForConditionalGeneration.from_pretrained(
+            self.model_id,
             torch_dtype=torch.bfloat16,
-            device_map="auto"
+            device_map="auto",
+            trust_remote_code=True,
         )
-        processor = AutoProcessor.from_pretrained(model_id)
+        processor = AutoProcessor.from_pretrained(self.model_id)
 
         return  model, processor
+
+    def train(self, data):
+        self.data = data
+
+        max_tokens = 2000
+
+        image_tensor = None
+
+        message = [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image",
+                        "image": image_tensor,
+                        "min_pixels": 512 * 512,
+                        "max_pixels": 14 * 14 * 1024 * 1024
+                    },
+                    {"type": "text", "text": self.prompt}
+                ]
+            }
+        ]
