0a76b27fc6059cf8e7107475bc2da05beb41d8b7
look-luc <denardiluc@gmail.com>
2026-07-31 13:23:12 -0600

    Update data_collector.py

--- src/model/urdu_dialect/data_collector.py
+++ src/model/urdu_dialect/data_collector.py
@@ -3,9 +3,42 @@ class QwenDataCollator:
         self.processor = processor
 
     def __call__(self, features):
+        text_str = []
+        imgs = []
+
+        for feature in features:
+            if "image" in feature:
+                imgs.append(feature.get("image"))
+            elif "images" in feature:
+                imgs.append(feature.get("images"))
+
+            raw_txt = feature.get("text")
+
+            if isinstance(raw_txt, list) and all(isinstance(item, dict) for item in raw_txt):
+                formated_text = self.processor.apply_chat_template(
+                    raw_txt,
+                    tokenize=False,
+                    add_generation_prompt=False
+                )
+            elif isinstance(raw_txt, dict):
+                key = next(
+                    (
+                        k for k in ["text", "content", "target"] if k in raw_txt
+                    ),
+                    None
+                )
+                if key is not None:
+                    formated_text = raw_txt[key]
+                else:
+                    formated_text = ""
+            else:
+                formated_text = raw_txt
+
+            text_str.append(formated_text)
+
         batch = self.processor(
-            images=[feature["image"] for feature in features],
-            text=[feature["text"] for feature in features],
+            images=imgs,
+            text=text_str,
             padding=True,
             return_tensors="pt",
         )
