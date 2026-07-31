9a35a92ff5e99df537f1877851a2e976422953a3
look-luc <denardiluc@gmail.com>
2026-07-31 11:05:04 -0600

    Update model.py

--- src/model/urdu_dialect/model.py
+++ src/model/urdu_dialect/model.py
@@ -19,9 +19,9 @@ root_dir = Path(__file__).resolve().parents[3]
 if str(root_dir) not in sys.path:
     sys.path.append(str(root_dir))
 
-from data_collector import QwenDataCollator
-
 from data.get_data import IMAGE_BASE_DIR, get_datasets
+
+from .data_collector import QwenDataCollator
 
 cer_metric = evaluate.load("cer")
 wer_metric = evaluate.load("wer")
