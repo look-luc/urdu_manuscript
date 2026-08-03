0fd1bda84efe8c5573a402d81326b6d4617a9f42
look-luc <denardiluc@gmail.com>
2026-08-03 16:57:57 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -33,8 +33,6 @@ module  load cuda
 module load anaconda
 conda activate urdu_manuscript_stable
 
-export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
-
 cd /projects/$USER/urdu_manuscript
 
 MODEL_TYPE=${1:-text_extraction}
