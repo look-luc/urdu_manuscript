c6c85d93c5e54d182ef89c04fd87d3e39f147892
look-luc <denardiluc@gmail.com>
2026-08-14 16:10:24 -0600

    testing checkpoint 1050 to see if it's better

--- src/model/text_extraction/model.py
+++ src/model/text_extraction/model.py
@@ -15,7 +15,7 @@ class text_extraction:
         self,
         model_id: str = "Qwen/Qwen2.5-VL-3B-Instruct",
         prompt: str = """You are an expert OCR model for historical Urdu and Arabic-script manuscripts with expert knowledge of Farsi/Persian, Arabic and Urdu. Transcribe the text line-by-line. If there are marginal notes or footnotes, transcribe them separately at the end under 'Marginalia'. Do not translate.""",
-        path_to_model:str=f"{script_path}/urdu_manuscript_model"
+        path_to_model:str=f"{script_path}/checkpoint-1050"
     ) -> None:
         torch.backends.cudnn.enabled = False
 
